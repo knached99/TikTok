c5608963ac735e1d962ac60ffc0aa0633bc2877a
Khaled Nached <khalednached@Khaleds-MacBook-Pro.local>
2021-07-01 20:00:41 -0400

    changed file layout

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>You have gained 250,900 followers on TikTok! Congratulations on becoming a TikTok Star!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontWeight: 'bold',
+    color: '#fff',
+  }
 });
